04e5c8ad7aa79e1d225f704182330ac0e8846b9f
Cacio de Castro <cacio.castro@gmail.com>
2021-08-13 17:43:51 -0300

    criação do middleware

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-
-@Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(), AuthModule],
-})
-export class AppModule {}
