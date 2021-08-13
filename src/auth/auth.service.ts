083ebf164dfb806d07e38c4ce508783a4dc225da
Jessica Marques <jessica.marques.dev@gmail.com>
2021-08-13 15:49:43 -0300

    Parte da autenticação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,10 +7,17 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(email);
-    console.log(user);
     if (user && user.password === password) {
-      const {password, ...res} = user;
-      return res;
+      const response = {
+        user: {
+          public_id: user.public_id,
+          name: user.name,
+          email: user.email,
+        },
+        access_token: user.access_token,
+      };
+  
+      return response;
     }
     return null;
   }
