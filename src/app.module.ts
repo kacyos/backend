e1492f92b724f35da9e47374bdce2a168d6c1024
Guilherme <guilherme@nfe.io>
2021-08-16 13:27:50 -0300

    orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PropostaModule } from './proposta/proposta.module';
 
 @Module({
-  imports: [PropostaModule],
+  imports: [
+    PropostaModule,
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
--- src/proposta/proposta.module.ts
+++ src/proposta/proposta.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PropostaService } from './proposta.service';
 import { PropostaController } from './proposta.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Proposta } from './entities/proposta.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Proposta])],
   controllers: [PropostaController],
   providers: [PropostaService]
 })
