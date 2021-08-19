7e1d3788e7cab91ae1cef75eed02052c6e67a68d
Guilherme <guilherme@nfe.io>
2021-08-19 11:45:00 -0300

    typeorm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,21 @@ import { ProposalModule } from './proposal/proposal.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ProposalModule, TypeOrmModule.forRoot()],
+  imports: [
+    UserModule, 
+    AuthModule, 
+    ProposalModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || "localhost",
+      port: 5432,
+      username: process.env.POSTGRES_USER || "postgres",
+      password: process.env.POSTGRES_PASSWORD || "mysecretpassword",
+      database: process.env.POSTGRES_DATABASE || "omega",
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
