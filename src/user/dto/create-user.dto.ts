785526872623271d5e09f2f066390b28e865eaf3
Guilherme <guilherme@nfe.io>
2021-08-19 11:11:09 -0300

    refactor user dto messages

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,13 +2,13 @@ import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  @MinLength(3, { message: 'O nome deve conter no mínimo 3 caracteres' })
+  @MinLength(3, { message: '$property deve conter no mínimo 3 caracteres' })
   readonly name: string;
 
-  @IsEmail({}, { message: 'O email informado é inválido' })
+  @IsEmail({}, { message: '$property é inválido' })
   readonly email: string;
 
   @IsString()
-  @MinLength(8, { message: 'A senha deve conter no mínimo 8 caracteres' })
+  @MinLength(8, { message: '$property deve conter no mínimo 8 caracteres' })
   readonly password: string;
 }
