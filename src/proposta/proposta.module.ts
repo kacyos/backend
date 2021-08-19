86d28d0ee82a5a5909dd39254e8ecd2582e1e359
Guilherme <guilherme@nfe.io>
2021-08-19 10:01:39 -0300

    fix auth

--- src/proposta/proposta.module.ts
+++ src/proposta/proposta.module.ts
@@ -5,10 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Proposta } from './entities/proposta.entity';
 import { Cargas } from './entities/cargas.entity';
 import { UsuarioModule } from 'src/usuario/usuario.module';
-import { UsuarioService } from 'src/usuario/usuario.service';
+import { jwtConstants } from 'src/auth/constants';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [UsuarioModule, TypeOrmModule.forFeature([Proposta, Cargas])],
+  imports: [
+    UsuarioModule,
+    TypeOrmModule.forFeature([Proposta, Cargas]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    })
+  ],
   controllers: [PropostaController],
   providers: [PropostaService],
 })
-export class PropostaModule {}
+export class PropostaModule { }
